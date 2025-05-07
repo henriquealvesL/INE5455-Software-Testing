db0d69587ecb0e727a744d94603682238158eae4
Henrique Alves <henriqueadlima@hotmail.com>
2025-05-07 09:41:27 -0300

    Fix create employee

--- Exercise-7-TDD-Part1/models/company.py
+++ Exercise-7-TDD-Part1/models/company.py
@@ -1,3 +1,10 @@
+from models.employee import Employee
+
 class Company():
   def __init__(self, name):
     self.name = name
+    self.employees = []
+
+  def create_employee(self, name):
+    employee = Employee(name)
+    self.employees.append(employee)
