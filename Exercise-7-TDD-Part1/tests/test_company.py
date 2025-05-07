d35e0cbd5e60b4d17dd2a5797ae77518c836adee
Henrique Alves <henriqueadlima@hotmail.com>
2025-05-07 09:31:07 -0300

    Rename tests

--- Exercise-7-TDD-Part1/tests/test_company.py
+++ Exercise-7-TDD-Part1/tests/test_company.py
@@ -2,10 +2,11 @@ import pytest
 
 from models.company import Company
 
-def test_create_company_w():
+def test_create_company_w1():
   company_w = Company("W's SA")
   assert company_w.name == "W's SA"
 
-def test_cretae_company_without_name():
+def test_create_company_without_name2():
   with pytest.raises(TypeError):
     Company()
+
