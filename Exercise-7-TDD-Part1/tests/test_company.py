436276691136537df4fb59cec944fa218f73c6ce
Henrique Alves <henriqueadlima@hotmail.com>
2025-05-07 09:29:54 -0300

    Add test create company without name

--- Exercise-7-TDD-Part1/tests/test_company.py
+++ Exercise-7-TDD-Part1/tests/test_company.py
@@ -1,5 +1,11 @@
+import pytest
+
 from models.company import Company
 
 def test_create_company_w():
   company_w = Company("W's SA")
   assert company_w.name == "W's SA"
+
+def test_cretae_company_without_name():
+  with pytest.raises(TypeError):
+    Company()
